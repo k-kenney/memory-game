9f33d53008490df5151f7a1fd35e318a7b01f9c9
Kayla Kenney <kaylachristinekenney@gmail.com>
2023-08-06 08:05:43 +0300

    changed title

--- src/App.jsx
+++ src/App.jsx
@@ -73,7 +73,7 @@ useEffect(() => {
 
   return (
     <div className="App">
-      <h1>Magic Match</h1>
+      <h1>Magic Memory</h1>
       <button onClick={shuffleCards}>New Game</button>
 
       <div className="card-grid">
